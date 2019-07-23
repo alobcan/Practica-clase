25b155241fa4a93c69677d510633210fede66bc7
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2019-07-23 00:56:40 -0400

    First commit"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Componente1Component } from './componente1/componente1.component';
+import { Directiva1Directive } from './directivas/directiva1.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component,
+    Directiva1Directive
   ],
   imports: [
     BrowserModule,
