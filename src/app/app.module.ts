243e7ecbe9cddab6f07ccd8e052cba0c1a44bd50
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2019-07-23 21:59:35 -0400

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Componente1Component } from './componente1/componente1.component';
 import { Directiva1Directive } from './directivas/directiva1.directive';
+import { StudentModule } from './student/student.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { Directiva1Directive } from './directivas/directiva1.directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StudentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
